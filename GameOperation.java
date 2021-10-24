5d1128baf78ba5806d7438644b7b2092f4bb68f6
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-10-24 12:56:40 +0530

    Choose a letter

--- GameOperation.java
+++ GameOperation.java
@@ -1,5 +1,7 @@
 package com.company1.Day7_TicTacToe;
 
+import java.util.Scanner;
+
 public class GameOperation {
     
     public static void printGameBoard(char [][] gameBoard) {
@@ -9,6 +11,27 @@ public class GameOperation {
             }
             System.out.println();
         }
+    }
 
+    public char player;
+    public char pc;
+    public void chooseLetter() {
+        System.out.print("Enter for player X or O : ");
+        Scanner scanInput = new Scanner(System.in);
+        char getUserInput = scanInput.next().charAt(0);
+
+        if (getUserInput == 'X' || getUserInput == 'O') {
+            if (getUserInput == 'X') {
+                this.player = 'X';
+                this.pc = 'O';
+            } else {
+                this.player = 'O';
+                this.pc = 'X';
+            }
+        } else {
+            System.out.println("wrong input");
+            chooseLetter();
+        }
+        System.out.println("player select - " + this.player + " pc select - " + this.pc);
     }
 }
--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -9,5 +9,6 @@ public class TicTacToeGame {
                               {'-','+','-','+','-' },
                               {' ','|',' ','|',' ' }};
         gameOperation.printGameBoard(gameBoard);
+        gameOperation.chooseLetter();
     }
 }
